452d056b21d54acedebf99eff85c5dcc79748216
Jihyun <hwiyu25@gmail.com>
2021-02-11 18:47:12 +0900

    Day5 마이너스 값 색 변화 추가

--- day5_210211/day5.js
+++ day5_210211/day5.js
@@ -2,16 +2,26 @@ const number = document.getElementById("number");
 const lowerBtn = document.getElementById("lower-btn");
 const addBtn = document.getElementById("add-btn");
 
+const changeColor = (num) => {
+  if (num < 0) {
+    number.style.color = "red";
+  } else {
+    number.style.color = "black";
+  }
+};
+
 const handleLowerBtn = () => {
   let currentNum = parseInt(number.textContent);
   currentNum -= 1;
   number.innerText = currentNum;
+  changeColor(currentNum);
 };
 
 const handleAddBtn = () => {
   let currentNum = parseInt(number.textContent);
   currentNum += 1;
   number.innerText = currentNum;
+  changeColor(currentNum);
 };
 
 const init = () => {
